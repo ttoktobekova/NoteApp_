658a09e8325b7ac80854639445a2598b955c409d
Tolgonai <gul775653@gmail.com>
2024-10-15 16:22:01 +0600

    Исправила ошибку . ObBoard показываеться 1 раз

--- app/src/main/java/com/example/noteapp/ui/fragment/onBoard/OnBoardPagingFragment.kt
+++ app/src/main/java/com/example/noteapp/ui/fragment/onBoard/OnBoardPagingFragment.kt
@@ -12,6 +12,7 @@ class OnBoardPagingFragment : Fragment() {
 
     private var _binding: FragmentOnBoardPagingBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
